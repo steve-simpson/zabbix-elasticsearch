5e3ee046dad94023392cc4517defde07e5352c43
Steve Simpson <stephen.simpson1991@gmail.com>
2019-11-18 16:49:51 +0000

    Fixed tests

--- tests/test_zabbix_elasticsearch.py
+++ tests/test_zabbix_elasticsearch.py
@@ -10,6 +10,6 @@ class TestArgParse:
         """ Parse default arguments and check for the defaults values in config file """
         args = parse_conf()
         # Ensure using the default configuration
-        assert args.conf_file == "conf/default.conf"
+        assert args.conf_file == "docs/default.conf"
         # Ensure loglevel is set correctly
         assert args.loglevel == 'INFO'
