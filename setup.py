0dae4cbf7a5d56ef4902a995a2045db4110e65a1
Steve Simpson <stephen.simpson1991@gmail.com>
2019-11-19 14:56:03 +0000

    Increased version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zabbix_elasticsearch",
-    version="0.1.1",
+    version="0.1.3",
     author="Steve Simpson",
     author_email="stephen.simpson1991@gmail.com",
     description="Zabbix Monitoring for Elasticsearch",
