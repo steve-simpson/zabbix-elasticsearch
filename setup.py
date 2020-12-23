f5181933424aa9df467dcb36b6f4397df92d1142
Steve Simpson <stephen.simpson1991@gmail.com>
2020-12-23 11:31:47 +0000

    bumped to 0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zabbix_elasticsearch",
-    version="0.5.0",
+    version="0.5.1",
     author="Steve Simpson",
     author_email="stephen.simpson1991@gmail.com",
     description="Zabbix Monitoring for Elasticsearch",
